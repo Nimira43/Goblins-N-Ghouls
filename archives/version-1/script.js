d0d3b338d4339260f5dcb2ee45a3670dab60ea3a
Nimira43 <n.rabone72@aol.com>
2025-12-29 14:50:56 +0000

    Archiving current game code.

---
+++ archives/version-1/script.js
@@ -0,0 +1,88 @@
+const screens = document.querySelectorAll('.screen')
+const chooseEnemyBtns = document.querySelectorAll('.choose-enemy-btn')
+const startBtn = document.getElementById('start-btn')
+const gameContainer = document.getElementById('game-container')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
+
+let seconds = 0
+let score = 0
+let selectedEnemy = null
+let timer = null
+
+startBtn.addEventListener('click', () => screens[0].classList.add('up'))
+
+chooseEnemyBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const img = btn.querySelector('img')
+    selectedEnemy = {
+      src: img.getAttribute('src'),
+      alt: img.getAttribute('alt')
+    }
+
+    screens[1].classList.add('up')
+    setTimeout(createEnemy, 1000)
+    startGame()
+  })
+})
+
+function startGame() {
+  if (!timer) {
+    timer = setInterval(increaseTime, 1000)
+  }
+}
+
+function increaseTime() { 
+  let m = Math.floor(seconds / 60)
+  let s = seconds % 60
+  m = m < 10 ? `0${m}` : m
+  s = s < 10 ? `0${s}` : s
+  timeEl.innerHTML = `Time: ${m}:${s}`
+  seconds++
+}
+
+function createEnemy() {
+  const enemy = document.createElement('div')
+  enemy.classList.add('enemy')
+  const { x, y } = getRandomLocation()
+  enemy.style.top = `${y}px`
+  enemy.style.left = `${x}px`
+  enemy.innerHTML = `
+    <img
+      src='${selectedEnemy.src}' 
+      alt='${selectedEnemy.alt}' 
+      style='transform: rotate(${Math.random() * 360}deg)'
+    />
+  `
+  enemy.addEventListener('click', catchEnemy)
+  gameContainer.appendChild(enemy)
+}
+
+function getRandomLocation() {
+  const width = window.innerWidth
+  const height = window.innerHeight
+  const x = Math.random() * (width - 200) + 100
+  const y = Math.random() * (height - 200) + 100
+  return { x, y }
+}
+
+function catchEnemy() { 
+  increaseScore()
+  this.classList.add('caught')
+  setTimeout(() => this.remove(), 2000)
+  addEnemies()
+}
+
+function addEnemies() { 
+  setTimeout(createEnemy, 1000)
+  setTimeout(createEnemy, 1500)
+}
+
+function increaseScore() {
+  score++
+  if (score > 19) {
+    message.classList.add('visible')
+  }
+  scoreEl.innerHTML = `Score: ${score}`
+}
