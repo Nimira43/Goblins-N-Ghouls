7571e1f2bdc442aeb025de6c3744f07694cb7cf1
Nimira43 <n.rabone72@aol.com>
2025-12-29 13:44:52 +0000

    const variables

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const screens = document.querySelectorAll('.screen')
+const chooseEnemyBtns = document.querySelectorAll('.choose-enemy-btn')
+const startBtn = document.getElementById('start-btn')
+const gameContainer = document.getElementById('game-container')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
 
 screens[0].classList.add('up')
